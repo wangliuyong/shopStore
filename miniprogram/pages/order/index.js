b728c13b5c80206f368c9ff7242380fcb86e1a23
wly <1355498705@qq.com>
2018-10-31 19:28:49 +0800

    添加地址栏输入框

--- miniprogram/pages/order/index.js
+++ miniprogram/pages/order/index.js
@@ -41,6 +41,103 @@ Page({
     })
   },
   onSubmit(){
+    this.payRuqest(100)
+  },
+  toAdd(){
+    wx.navigateTo({
+      url: '/pages/address/index'
+    })
+  },
+  //支付
+  payRuqest(needPay) {
+    var _this=this;
+    let tradeNo = new Date().getTime();
+    let openid = ''
+    //调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {}
+    }).then((res) => {
+      openid = res.result.openid
+      sendPay();
+    })
 
+    //发送支付请求
+    function sendPay() {
+      //向后端请求数据
+      wx.request({
+        url: 'https://xcx.ioobot.com/payinfo',
+        data: {
+          needPay,
+          openid,
+          tradeNo
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success(res) {
+          if (res.data) {
+            requestPayment(res)
+          }
+        },
+        fail(res) {
+          wx.showToast({
+            title: '支付失败'
+          })
+        }
+      })
+    }
+    //调起支付
+    function requestPayment(res) {
+      let {
+        nonceStr,
+        paySign,
+        timeStamp,
+        signType
+      } = res.data;
+      let package1 = res.data.package;
+      //package是严格模式下的保留字
+      wx.requestPayment({
+        'timeStamp': timeStamp,
+        'nonceStr': nonceStr,
+        'package': package1,
+        'signType': signType,
+        'paySign': paySign,
+        'success': function (res) {
+          wx.showToast({
+            title: '支付成功'
+          })
+          //支付成功之将预定成功的杂志数据发送到后端      
+          _this.setData({
+            "order.orderStatus":"finishPay"
+          })
+          _this.createOrder(_this.data.order)
+
+          console.log(_this.data.order)
+
+          setTimeout(()=>{
+            wx.switchTab({
+              url: '/pages/my/my'
+            })
+            app.globalData.pay=true
+          },1000)
+        },
+        'fail': function (res) {
+          wx.showToast({
+            title: '支付失败'
+          })
+
+          _this.createOrder(_this.data.order)
+          console.log(_this.data.order)
+          setTimeout(()=>{
+            wx.switchTab({
+              url: '/pages/my/my?pay=false'
+            })
+            app.globalData.pay=false
+          },1000)
+        },
+        'complete': function (res) {}
+      })
+    }
   }
 })
